23c52e45813c7eb79372c8d4c4fb2cd31c636425
Luke Williamson <lukewilliamson1995@gmail.com>
2017-05-19 14:49:22 +0100

    started again with the loader

--- js/script.js
+++ js/script.js
@@ -6,5 +6,5 @@ function myFunction() {
 
 function showPage() {
   document.getElementById("loader").style.display = "none";
-  document.getElementById("welcome").style.display = "block";
+  document.getElementById("myDiv").style.display = "block";
 }
