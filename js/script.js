bd959bc8d3b09cedfd6d92a31615fbab7da8ec72
Luke Williamson <lukewilliamson1995@gmail.com>
2017-05-22 13:36:55 +0100

    added extra styling to profile pic and changed class and id names

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ function myFunction() {
 
 function showPage() {
   document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
+  document.getElementById("introduction").style.display = "block";
 }
 
 myFunction();
